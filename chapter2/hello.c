fca6f97112db1106eb425c1b498667592bcd8a21
block0xhash <bothwellw@gmail.com>
2025-05-20 09:57:17 +0200

    add boolean

--- chapter2/hello.c
+++ chapter2/hello.c
@@ -2,8 +2,25 @@
 /* hello world program */
 
 #include <stdio.h>
+#include <stdbool.h> // not needed in C23
 
 int main(void) {
-	printf("hello world\n"); // actuall do some work
+
+	int i [[maybe_unused]];   // holds signed integers like -3, -2, 0, 10
+	float f [[maybe_unused]]; // holds a signed floating point number like -3.1416
+
+	i = 2;
+	f = 3.14;
+	char *s = ("hello, World"); // char * ("char pointer") is the string type 
+
+	printf("%p %s i= %d and f = %f \n",s, s, i , f);  // actuall do some work
+
+
+	/* Bolean */
+	int b = 1;
+
+	if (b) {
+		printf("b is true \n");
+	}
 
 }
